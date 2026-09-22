6ffeb3a31c917bd1b90e305e508628e544563711
mthan <me@mthan.dev>
2026-09-22 13:19:03 +0300

    add the ssh text (final commit)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/ed25519"
 	"crypto/rand"
 	"encoding/pem"
@@ -21,20 +22,45 @@ func main() {
 	addr := flag.String("addr", ":22", "address to listen on")
 	httpAddr := flag.String("http", ":80", "address for the HTTP redirect server")
 	hostKeyPath := flag.String("hostkey", "host_ed25519", "path to the server's host key (created if missing)")
+	textPath := flag.String("text", "text.txt", "file whose contents are sent to SSH clients")
+	logPath := flag.String("log", "server.log", "file that connections and requests are logged to")
 	flag.Parse()
+
+	logFile, err := os.OpenFile(*logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	if err != nil {
+		log.Fatalf("log file: %v", err)
+	}
+	defer logFile.Close()
+	log.SetOutput(io.MultiWriter(os.Stderr, logFile))
 
 	if err := ensureHostKey(*hostKeyPath); err != nil {
 		log.Fatalf("host key: %v", err)
 	}
 
 	ssh.Handle(func(s ssh.Session) {
-		log.Printf("connection from %s (user %q)", s.RemoteAddr(), s.User())
-		io.WriteString(s, "Hello, world!\n")
+		log.Printf("ssh: connection from %s (user %q, command %q)", s.RemoteAddr(), s.User(), s.RawCommand())
+		// Read on every connection so edits to the file show up without a restart.
+		text, err := os.ReadFile(*textPath)
+		if err != nil {
+			log.Printf("ssh: reading %s: %v", *textPath, err)
+			io.WriteString(s, "Hello, world!\n")
+			return
+		}
+		// With a PTY the client's terminal is in raw mode, so bare \n would
+		// move down without returning to column 0.
+		if _, _, isPty := s.Pty(); isPty {
+			text = bytes.ReplaceAll(text, []byte("\n"), []byte("\r\n"))
+		}
+		s.Write(text)
 	})
 
+	redirect := http.RedirectHandler(redirectURL, http.StatusFound)
 	go func() {
 		log.Printf("http listening on %s", *httpAddr)
-		log.Fatal(http.ListenAndServe(*httpAddr, http.RedirectHandler(redirectURL, http.StatusFound)))
+		log.Fatal(http.ListenAndServe(*httpAddr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("http: %s %s %s from %s (user agent %q)", r.Method, r.Host, r.URL, r.RemoteAddr, r.UserAgent())
+			redirect.ServeHTTP(w, r)
+		})))
 	}()
 
 	log.Printf("listening on %s", *addr)
